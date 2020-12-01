e7d3fd2c370a4934e05f86b17c5cc7c645099eaf
Yulei_Zhang <zc_1994@163.com>
2020-12-01 14:12:48 +0800

    Modified DNN

--- Algorithms/RecECAL/src/ECAL_RNN.cpp
+++ Algorithms/RecECAL/src/ECAL_RNN.cpp
@@ -109,7 +109,6 @@ double ECAL_RNN::ApplyDNN(const shared_ptr<vector<SimulatedHit *>> &hits) {
     for (auto hit : *hits) {
         int cell_id = hit->getCellId();
         Hits.at(cell_id-1) = ( hit->getE() > 1e-6 && !isnan(hit->getE()) ) ? hit->getE() : 0. ;
-        cout<<Hits.at(cell_id-1)<<endl;
     }
     return reader->EvaluateMVA(DatasetName);
 }
