efe8c6de2b996ca336e779fd0c41a15b430abb94
qibin.liu@cern.ch <liuqibin@bl-0.phy.sjtulocal>
2020-12-16 14:09:12 +0800

    Use default ECAL center design for optical study.

--- DP_simu/src/DetectorConstruction.cc
+++ DP_simu/src/DetectorConstruction.cc
@@ -81,7 +81,7 @@ DetectorConstruction::DetectorConstruction(RootManager *rootMng) {
     build_RecTrk = true;
     build_HCAL = true;
 
-    ECAL_Selection = 2;
+    ECAL_Selection = 1;
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
