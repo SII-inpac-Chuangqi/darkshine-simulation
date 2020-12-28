bc1accc91a0081f0ad089717ba7ddde7ffc348a3
Yulei_Zhang <zc_1994@163.com>
2020-12-28 22:50:07 +0800

    test for memory leak

--- DP_simu/src/TrackingAction.cc
+++ DP_simu/src/TrackingAction.cc
@@ -37,6 +37,8 @@
 #include "DP_simu/RootManager.hh"
 #include "G4SystemOfUnits.hh"
 
+#include "TStorage.h"
+
 class MCParticle;
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -55,41 +57,41 @@ TrackingAction::~TrackingAction() {
 
 void TrackingAction::PreUserTrackingAction(const G4Track *aTrack) {
     /* Initialize Filter */
-    //froot->Filter_Initialize();
-//    fMC = new McParticle();
-//    fMC->setPdg(aTrack->GetParticleDefinition()->GetPDGEncoding());
-//    fMC->setId(aTrack->GetTrackID());
-//    fMC->setEnergy(aTrack->GetKineticEnergy());
-//    fMC->setPx(aTrack->GetMomentum()[0]);
-//    fMC->setPy(aTrack->GetMomentum()[1]);
-//    fMC->setPz(aTrack->GetMomentum()[2]);
-//    fMC->setVertexX(aTrack->GetPosition()[0]);
-//    fMC->setVertexY(aTrack->GetPosition()[1]);
-//    fMC->setVertexZ(aTrack->GetPosition()[2]);
+    fMC = new McParticle();
+    fMC->setPdg(aTrack->GetParticleDefinition()->GetPDGEncoding());
+    fMC->setId(aTrack->GetTrackID());
+    fMC->setEnergy(aTrack->GetKineticEnergy());
+    fMC->setPx(aTrack->GetMomentum()[0]);
+    fMC->setPy(aTrack->GetMomentum()[1]);
+    fMC->setPz(aTrack->GetMomentum()[2]);
+    fMC->setVertexX(aTrack->GetPosition()[0]);
+    fMC->setVertexY(aTrack->GetPosition()[1]);
+    fMC->setVertexZ(aTrack->GetPosition()[2]);
 
-//    if (aTrack->GetCreatorProcess())
-//        fMC->setCreateProcess(aTrack->GetCreatorProcess()->GetProcessName());
-//
-//    G4double pm = sqrt(fMC->getPx() * fMC->getPx() +
-//                       fMC->getPy() * fMC->getPy() +
-//                       fMC->getPz() * fMC->getPz());
-//
-//    auto MC = new McParticle(*fMC);
-//
-//    if (fMC->getId() == 1
-//        || pm >= 1. * GeV
-//        || (fMC->getEnergy() >= 1. * GeV && fMC->getEnergy() <= 8. * GeV)
-//        || abs(fMC->getPdg()) == 13   // Muon
-//        || abs(fMC->getPdg()) == 111  // Pion0
-//        || abs(fMC->getPdg()) == 211  // Pion+-
-//        || abs(fMC->getPdg()) == 321  // Kaon+-
-//        || abs(fMC->getPdg()) == 2212 // proton
-//        || abs(fMC->getPdg()) == 2112 // neutron
-//        || abs(fMC->getPdg()) == 14   // muon neutrino
-//        || abs(fMC->getPdg()) == 12   // electron neutrino
-//            )
-//        froot->FillMC(MC, aTrack->GetParentID());
+    if (aTrack->GetCreatorProcess())
+        fMC->setCreateProcess(aTrack->GetCreatorProcess()->GetProcessName());
 
+    G4double pm = sqrt(fMC->getPx() * fMC->getPx() +
+                       fMC->getPy() * fMC->getPy() +
+                       fMC->getPz() * fMC->getPz());
+
+    auto MC = new McParticle(*fMC);
+
+    if (fMC->getId() == 1
+        || pm >= 1. * GeV
+        || (fMC->getEnergy() >= 1. * GeV && fMC->getEnergy() <= 8. * GeV)
+        || abs(fMC->getPdg()) == 13   // Muon
+        || abs(fMC->getPdg()) == 111  // Pion0
+        || abs(fMC->getPdg()) == 211  // Pion+-
+        || abs(fMC->getPdg()) == 321  // Kaon+-
+        || abs(fMC->getPdg()) == 2212 // proton
+        || abs(fMC->getPdg()) == 2112 // neutron
+        || abs(fMC->getPdg()) == 14   // muon neutrino
+        || abs(fMC->getPdg()) == 12   // electron neutrino
+            )
+        froot->FillMC(MC, aTrack->GetParentID());
+
+    delete fMC;
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
