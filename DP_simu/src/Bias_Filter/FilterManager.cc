83570d61db0daeb7a023863bcc90b79053f1d80c
zhuxu <zhuxuliang@outlook.com>
2020-12-30 16:04:43 +0800

    fixing Filter bug

--- DP_simu/src/Bias_Filter/FilterManager.cc
+++ DP_simu/src/Bias_Filter/FilterManager.cc
@@ -18,15 +18,20 @@ G4bool FilterManager::Filter_Particle(const G4Step* aStep) {
             }
             // else,
         }
-        else { // not found this particle in particular range.
-            if (fFilterParticle->GetFlag()) { // but this particle is must-have.
-                Filter_Particle_Result = false;
-                return false;
-            }
-        }
     }
     Filter_Particle_Result = true; // found all particle we want to see, and no particle we don't want.
     return Filter_Particle_Result;
+}
+
+G4bool FilterManager::Filter_Particle_EndofEvent() {
+    particle_end = Filter_Particle_List.end();
+    for (particle_itr = Filter_Particle_List.begin(); particle_itr != particle_end; particle_itr++) {
+        std::shared_ptr<FilterParticle> fFilterParticle = *particle_itr;
+        if ( fFilterParticle->GetFlag() && !fFilterParticle->GetFoundResult() ) { // must have this particle but not found
+            return false; // filter
+        }
+    }
+    return true; // keep
 }
 
 /// \brief Filter Process method. Scan every FilterProcess->Filter().
@@ -41,14 +46,19 @@ void FilterManager::Filter_Process(const G4Step* aStep) {
                 return;
             }
         }
-        else { // not found process in particular range
-            if ( fFilterProcess->GetFlag() ) { // but must have this process
-                Filter_Process_Result = false;
-                return;
-            }
-        }
     }
     Filter_Process_Result = true;
+}
+
+G4bool FilterManager::Filter_Process_EndofEvent() {
+    process_end = Filter_Process_List.end();
+    for (process_itr = Filter_Process_List.begin(); process_itr != process_end; process_itr++) {
+        std::shared_ptr<FilterProcess> fFilterProcess = *process_itr;
+        if ( fFilterProcess->GetFlag() && !fFilterProcess->GetFoundResult() ) { // must have this process but not found
+            return false; // filter
+        }
+    }
+    return true; // keep
 }
 
 void FilterManager::Filter_Track_Initialize() {
@@ -58,7 +68,21 @@ void FilterManager::Filter_Track_Initialize() {
 void FilterManager::Filter_Event_Initialize() {
     Filter_Particle_Result = true;
     Filter_Process_Result = true;
+    // set Found_Result of particle to fasle
+    particle_end = Filter_Particle_List.end();
+    for (particle_itr = Filter_Particle_List.begin(); particle_itr != particle_end; particle_itr++) {
+        std::shared_ptr<FilterParticle> fFilterParticle = *particle_itr;
+        fFilterParticle->SetFoundResult(false);
+    }
+    // set Found_Result of process to false
+    process_end = Filter_Process_List.end();
+    for (process_itr = Filter_Process_List.begin(); process_itr != process_end; process_itr++) {
+        std::shared_ptr<FilterProcess> fFilterProcess = *process_itr;
+        fFilterProcess->SetFoundResult(false);
+    }
 }
+
+
 
 void FilterManager::SetNew_Particle_Filter(G4int pdg, G4double risingEnergyEdge, G4double fallingEnergyEdge,
                                            G4double risingScanEdge, G4double fallingScanEdge, G4bool flag)
