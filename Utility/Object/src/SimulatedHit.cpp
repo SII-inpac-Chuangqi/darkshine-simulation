2ceeadbce5f8533affee4d55f9aeb194ea509b4c
Yulei_Zhang <zc_1994@163.com>
2021-04-09 11:05:59 +0800

    change MCP Contribution to record all hadrons

--- Utility/Object/src/SimulatedHit.cpp
+++ Utility/Object/src/SimulatedHit.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "Object/SimulatedHit.h"
+#include "G4SystemOfUnits.hh"
 
 #include <cassert>
 
@@ -76,11 +77,36 @@ bool SimulatedHit::operator!=(const SimulatedHit &rhs) const {
 }
 
 // Add the 3 particles with the most energy deposition contributed to this hit
-void SimulatedHit::addParticleContribution(const McParticle& mcp, double Edep, bool record_all) {
-    if (MCPContribution.size() >= 3 && !record_all) {
+void SimulatedHit::addParticleContribution(const McParticle &mcp, double Edep, bool record_all) {
+    auto pdg = mcp.getPdg();
+    bool target_mcp = (mcp.getId() == 1
+                       || abs(pdg) == 13   // Muon
+                       || abs(pdg) == 14   // muon neutrino
+                       || abs(pdg) == 12   // electron neutrino
+                       || (abs(pdg) >= 100 && abs(pdg) <= 10000) // inclusive hadrons
+    );
+
+    // merge two same particle contribution
+    for (unsigned i = 0; i < SimHits_Edep.size(); ++i) {
+        if (MCPContribution.at(i).getId() == mcp.getId()) {
+            SimHits_Edep.at(i) += Edep;
+            return;
+        }
+    }
+
+    // update MCP Contribution with the top 5 deposited energy particles
+    // unless they are targeted particle
+    if (MCPContribution.size() >= 5 && !record_all && !target_mcp) {
         assert(SimHits_Edep.size() == MCPContribution.size());
         for (unsigned i = 0; i < SimHits_Edep.size(); ++i) {
-            if (SimHits_Edep.at(i) < Edep || mcp.getId() == 1) {
+            if (SimHits_Edep.at(i) < Edep
+                && !(MCPContribution.at(i).getId() == 1
+                     || abs(MCPContribution.at(i).getPdg()) == 13   // Muon
+                     || abs(MCPContribution.at(i).getPdg()) == 14   // muon neutrino
+                     || abs(MCPContribution.at(i).getPdg()) == 12   // electron neutrino
+                     || (abs(MCPContribution.at(i).getPdg()) >= 100 &&
+                         abs(MCPContribution.at(i).getPdg()) <= 10000) // inclusive hadrons
+            )) {
                 MCPContribution.at(i) = McParticle(mcp);
                 SimHits_Edep.at(i) = Edep;
                 break;
