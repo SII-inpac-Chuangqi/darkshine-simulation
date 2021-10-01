f4030611e712dffb9cffae299e340e8ca49c8960
Yulei_Zhang <zc_1994@163.com>
2021-10-01 21:29:43 +0800

    require ecal hits collection not to be empty
    Took 19 seconds

--- Algorithms/RecECAL/src/RecECAL.cpp
+++ Algorithms/RecECAL/src/RecECAL.cpp
@@ -75,6 +75,7 @@ void RecECAL::ProcessEvt(AnaEvent *evt) {
         // IMPORTANT: check if the collection exists
         if (HitCollection.count(HitCollectionName) != 0) {
             const auto &hits = HitCollection.at(HitCollectionName);
+            if (hits->empty()) continue;
             // Calculate some cluster parameters ( moments...)
             auto cluster_ana = std::shared_ptr<Cluster_Analysis>(new Cluster_Analysis(hits));
             E_total.push_back(cluster_ana->FindETotal());
