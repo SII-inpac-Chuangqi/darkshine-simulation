18e8543b5589a3322752bce09aec0dc2d869ddeb
qibin.liu@cern.ch <liuqibin@bl-0.phy.sjtulocal>
2022-06-11 12:55:40 +0800

    [CI] Fix pipe error

--- Automaton/build.sh
+++ Automaton/build.sh
@@ -21,7 +21,7 @@ cd ${_DSS_TEST_PREFIX}/build
 ls -la # test cache
 cmake -DWITH_GEANT4_UIVIS=OFF -DCMAKE_INSTALL_PREFIX=${_DSS_TEST_PREFIX}/install -DCMAKE_BUILD_TYPE=RelWithDebInfo ../ |& tee ${_DSS_TEST_PREFIX}/test/DSS.build.log \
                 && make install -j16 |& tee -a ${_DSS_TEST_PREFIX}/test/DSS.build.log # 1>/dev/null
-_DSS_TEST_STATUS=$?
+_DSS_TEST_STATUS=${PIPESTATUS[0]}
 if [ ${_DSS_TEST_STATUS} -ne 0 ]; then
     diagnostic
     # cat ${_DSS_TEST_PREFIX}/test/DSS.build.log
--- DP_ana/scripts/test.cmd
+++ DP_ana/scripts/test.cmd
@@ -2,7 +2,7 @@ _obj=DAna
 _type=Export
 _log=${_obj}.${_type}.log
 DAna -x |$ tee ${_log} > DAna.test.config
-_DSS_TEST_STATUS=$?
+_DSS_TEST_STATUS=${PIPESTATUS[0]}
 if [ ${_DSS_TEST_STATUS} -ne 0 ]; then
     export _DSS_TEST_FATAL="${_type}"
     export _DSS_TEST_LOG="${PWD}/${_log}"
@@ -13,7 +13,7 @@ _obj=DAna
 _type=Test
 _log=${_obj}.${_type}.log
 DAna -c DAna.test.config |& tee ${_log} # 1>/dev/null
-_DSS_TEST_STATUS=$?
+_DSS_TEST_STATUS=${PIPESTATUS[0]}
 if [ ${_DSS_TEST_STATUS} -ne 0 ]; then
     export _DSS_TEST_FATAL="${_type}"
     export _DSS_TEST_LOG="${PWD}/${_log}"
@@ -25,7 +25,7 @@ _type=Draw
 _log=${_obj}.${_type}.log
 rootdrawtree -i dp_ana.root -t dp -hs "hECAL=ECAL_E_total[0]" -o DAna.hist.root |& tee ${_log} # 1>/dev/null
 rootprint -f png DAna.hist.root:hECAL |& tee -a ${_log} # 1>/dev/null
-_DSS_TEST_STATUS=$?
+_DSS_TEST_STATUS=${PIPESTATUS[0]}
 if [ ${_DSS_TEST_STATUS} -ne 0 ]; then
     export _DSS_TEST_FATAL="${_type}"
     export _DSS_TEST_LOG="${PWD}/${_log}"
