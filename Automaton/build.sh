14932e342a29a7d14a82b8c31648d5ebac0aed78
qibin.liu@cern.ch <liuqibin@bl-0.phy.sjtulocal>
2022-06-11 11:17:09 +0800

    [CI] Debug cache

--- Automaton/build.sh
+++ Automaton/build.sh
@@ -18,6 +18,7 @@ mkdir -p install
 mkdir -p test
 
 cd ${_DSS_TEST_PREFIX}/build
+ls -la # test cache
 cmake -DWITH_GEANT4_UIVIS=OFF -DCMAKE_INSTALL_PREFIX=${_DSS_TEST_PREFIX}/install -DCMAKE_BUILD_TYPE=RelWithDebInfo ../ |& tee ${_DSS_TEST_PREFIX}/test/DSS.build.log \
                 && make install -j16 |& tee -a ${_DSS_TEST_PREFIX}/test/DSS.build.log # 1>/dev/null
 _DSS_TEST_STATUS=$?
