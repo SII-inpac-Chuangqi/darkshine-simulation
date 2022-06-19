d889cd4f312103a0142347fcbbe7d629baf62a18
qibin.liu@cern.ch <liuqibin@bl-0.phy.sjtulocal>
2022-06-19 22:56:52 +0800

    Add make_wiki func  - Note: the upload script has key embeded so only compiled prog updated

--- Tools/scripts/test.cmd
+++ Tools/scripts/test.cmd
@@ -3,6 +3,7 @@ _type=ECAL
 _log=${_obj}.${_type}.log
 cp ${_DSS_TEST_ROOT}/ref.dp_ana.root .
 cp ${_DSS_TEST_ROOT}/validation.yaml .
+cp ${_DSS_TEST_ROOT}/validation.md .
 DPlot -c validation.yaml |& tee ${_log}
 _DSS_TEST_STATUS=${PIPESTATUS[0]}
 if [ ${_DSS_TEST_STATUS} -ne 0 ]; then
@@ -10,3 +11,4 @@ if [ ${_DSS_TEST_STATUS} -ne 0 ]; then
     export _DSS_TEST_LOG="${PWD}/${_log}"
     return
 fi
+
