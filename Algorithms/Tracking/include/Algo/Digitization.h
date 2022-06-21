5c964501f65314e8cb86b7bfaa53d480bf91aa81
Jonah Zhu <2557928898@qq.com>
2022-06-21 06:18:19 +0000

    Tracking

--- Algorithms/Tracking/include/Algo/Digitization.h
+++ Algorithms/Tracking/include/Algo/Digitization.h
@@ -8,7 +8,7 @@
 #endif
 
 #ifndef RETURN
-#define RETURN -INFINITY
+#define RETURN std::nan("RETURN")
 #endif
 
 //................................................................................//
@@ -43,15 +43,70 @@ public:
 //Get tracker information
     void GetTrackerInfo(bool if_strip);
 //Separate tracker hits into vectors by layers
-    void Layering(const std::vector<TrkHit> &trk1_hits, const std::vector<TrkHit> &trk2_hits, TrkHitPVecMap &clusTrkHitMap,
+    void Layering(const std::vector<TrkHit> &trk1_hits, const std::vector<TrkHit> &trk2_hits, TrkHitPVecMap &recoTrkHitMap,
                   int detector);
+//Set strip cluster width
+    void SetClusterWidth(double cluster_width) {cluster_width_ = cluster_width;}
 
 private:
+//................................................................................//
+//Struct to construct cluster
+    struct Point
+    {
+        double x{RETURN};
+        double w{RETURN};
+        bool cluster{false};
+    };
 
-    TGeoNode* world_;
+//................................................................................//
+//Cluster
+    class Cluster
+    {
+    public:
+        ~Cluster() {delete center; center = nullptr;}
+
+        double GetCenterX() const {return center->x;}
+
+        double GetNewCenterX()
+        {
+            double sum_of_w(0.);
+            double sum_of_x(0.);
+
+            for(const auto &point : points)
+            {
+                sum_of_w += point->w;
+                sum_of_x += point->w*point->x;
+            }
+
+            if(sum_of_w == 0.) return center->x;
+            return sum_of_x/sum_of_w;
+        }
+
+        void SetCenter(double x, double w)
+        {
+            if(center) {center->x = x; center->w = w;}
+            else
+            {
+                center = new Point({x, w});
+            }
+        }
+
+        Point *center = new Point();
+        std::vector<Point*> points;
+    };
+
+    void InitHitMap(const TrkHitPVec &trk_hits, TrkHitPVecMap &trk_hit_map);
+    void InsertHitMap(const TrkHitP &trk_hit, TrkHitPVecMap &trk_hit_map);
+
+    double GetClusterDistance(const Point &point1, const Point &point2) {return std::abs(point1.x - point2.x);}
+    int GetNextClusterSeed(const vector<Point> &points);
+    void MakeClusterPoints(const TrkHitPVec &trk_hits, std::vector<Point> &points);
+    void MakeCluster(std::vector<Point> &points, std::vector<Cluster*> &clusters);
 
     bool if_strip_{true};
     bool if_smear_{true};
+
+    double cluster_width_ = 0.1;
 
     std::vector<double> layer_width_tag_ = {};
     std::vector<double> layer_length_tag_ = {};
