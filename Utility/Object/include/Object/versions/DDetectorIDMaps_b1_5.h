d7745d3e40989fc462689953279ce6efdf6d8ce2
201700830004@mail.sdu.edu.cn <201700830004@mail.sdu.edu.cn>
2023-03-17 04:48:32 +0000

    Fix SensitiveDetector bug

--- Utility/Object/include/Object/versions/DDetectorIDMaps_b1_5.h
+++ Utility/Object/include/Object/versions/DDetectorIDMaps_b1_5.h
@@ -8,79 +8,79 @@
 #include <map>
 #include "TObject.h"
 #include "TVector3.h"
+#include "DHit.h"
 
 class DDetectorIDMaps_b1_5 : public TObject {
 public:
     DDetectorIDMaps_b1_5(const DDetectorIDMaps_b1_5 &) = delete;
     DDetectorIDMaps_b1_5 &operator=(DDetectorIDMaps_b1_5 const &) = delete;
 
-    static DDetectorIDMaps_b1_5 *CreateInstance();
+    static void CreateInstance();
 
     /// Getters
 
-    int GetECALIDX(int reNumber1, int reNumber2) {
-        return ECALIDXMap.at(std::make_pair(reNumber1, reNumber2));
+    int GetIDX(int reNumber1, int reNumber2) {
+        return IDXMap.at(std::make_pair(reNumber1, reNumber2));
     }
 
-    int GetECALIDY(int reNumber1, int reNumber2) {
-        return ECALIDYMap.at(std::make_pair(reNumber1, reNumber2));
+    int GetIDY(int reNumber1, int reNumber2) {
+        return IDYMap.at(std::make_pair(reNumber1, reNumber2));
     }
 
-    int GetECALIDZ(int reNumber1, int reNumber2) {
-        return ECALIDZMap.at(std::make_pair(reNumber1, reNumber2));
+    int GetIDZ(int reNumber1, int reNumber2) {
+        return IDZMap.at(std::make_pair(reNumber1, reNumber2));
     }
 
-    int GetHCALIDX(int reNumber) {
-        return HCALIDXMap.at(reNumber);
-    }
-
-    int GetHCALIDY(int reNumber) {
-        return HCALIDYMap.at(reNumber);
+    int GetID(int reNumber1, int reNumber2) {
+        return IDMap.at(std::make_pair(reNumber1, reNumber2));
     }
 
     /// Setters
 
-    void SetECALBlockNo(Double_t x, Double_t y, Double_t z) {
-        ECAL_Block_No = TVector3(x,y,z);
+    void SetBlockShape(Int_t x, Int_t y, Int_t z) {
+        Block_Shape = {x, y, z};
     }
 
-    void SetECALCellNo(Double_t x, Double_t y, Double_t z) {
-        ECAL_Cell_No = TVector3(x,y,z);
+    void SetCellShape(Int_t x, Int_t y, Int_t z) {
+        Cell_Shape = {x, y, z};
     }
 
-    void SetHCALModuleNo(Double_t x, Double_t y, Double_t z) {
-        HCAL_Module_No = TVector3(x,y,z);
-    }
+    void SetDetectorType(DetectorType in) {dType = in;}
 
-    void FillECALID(int reNumber1, int reNumber2, int idX, int idY,int idZ) {
-        ECALIDXMap[std::make_pair(reNumber1, reNumber2)] = idX;
-        ECALIDYMap[std::make_pair(reNumber1, reNumber2)] = idY;
-        ECALIDZMap[std::make_pair(reNumber1, reNumber2)] = idZ;
-    }
-
-    void FillHCALID(int reNumber, int idX, int idY) {
-        HCALIDXMap.insert(std::make_pair(reNumber, idX));
-        HCALIDYMap.insert(std::make_pair(reNumber, idY));
+    void FillDetectorID(int idX, int idY,int idZ, int id) {
+        IDXMap[std::make_pair(reNumber1, reNumber2)] = idX;
+        IDYMap[std::make_pair(reNumber1, reNumber2)] = idY;
+        IDZMap[std::make_pair(reNumber1, reNumber2)] = idZ;
+        IDMap[std::make_pair(reNumber1, reNumber2)] = id;
     }
 
     void BuildMap();
 
 private:
+    void LoopCellID(int blockX, int blockY, int blockZ);
+
+private:
     DDetectorIDMaps_b1_5() = default;
 
-    std::map<std::pair<int, int>, int> ECALIDXMap{};
-    std::map<std::pair<int, int>, int> ECALIDYMap{};
-    std::map<std::pair<int, int>, int> ECALIDZMap{};
+    std::map<std::pair<int, int>, int> IDXMap{};
+    std::map<std::pair<int, int>, int> IDYMap{};
+    std::map<std::pair<int, int>, int> IDZMap{};
+    std::map<std::pair<int, int>, int> IDMap{};
 
-    std::map<int, int> HCALIDXMap{};
-    std::map<int, int> HCALIDYMap{};
+    std::vector<int> Block_Shape;
+    std::vector<int> Cell_Shape;
+    int xID;
+    int yID;
 
-    TVector3 ECAL_Block_No;
-    TVector3 ECAL_Cell_No;
-    TVector3 HCAL_Module_No;
+    int reNumber1;
+    int reNumber2;
+    DetectorType dType;
 
 ClassDefOverride(DDetectorIDMaps_b1_5, 1);
 };
 
-extern DDetectorIDMaps_b1_5 *dDetectorIDMaps;
+extern DDetectorIDMaps_b1_5 *dECALIDMaps;
+extern DDetectorIDMaps_b1_5 *dHCALIDMaps;
+extern DDetectorIDMaps_b1_5 *dSideHCALIDMaps;
+
 #endif //DSIMU_DDETECTORIDMAPS_B1_5_H
