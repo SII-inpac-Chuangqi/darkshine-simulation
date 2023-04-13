041a785c81bee6aa8aa43404f4c11ea43afea872
Jonah Zhu <2557928898@qq.com>
2023-04-13 21:57:13 +0800

    add node in vertex cluster

--- Algorithms/Tracking/include/Algo/Vertex/VertexFinder.h
+++ Algorithms/Tracking/include/Algo/Vertex/VertexFinder.h
@@ -5,16 +5,19 @@
 #include <memory>
 
 #include "Algo/DTrack.h"
+#include "Algo/Vertex/DVertex.h"
+#include "Algo/Vertex/Cluster.h"
 
 class VertexFinder
 {
 public:
     VertexFinder() {}
-    VertexFinder(const std::vector<std::shared_ptr<DTrack>> &tracks) {}
+    VertexFinder(const std::vector<std::shared_ptr<DTrack>> &tracks);
 
     ~VertexFinder() {}
 
 private:
+    std::vector<std::shared_ptr<DTrack>> tracks_;
 };
 
 #endif
--- Algorithms/Tracking/src/Vertex/VertexFinder.cpp
+++ Algorithms/Tracking/src/Vertex/VertexFinder.cpp
@@ -1,1 +1,6 @@
 #include "Algo/Vertex/VertexFinder.h"
+
+VertexFinder::VertexFinder(const std::vector<std::shared_ptr<DTrack>> &tracks)
+{
+    tracks_ = tracks;
+}
