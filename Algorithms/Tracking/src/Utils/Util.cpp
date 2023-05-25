4f07b6ce5eaa0d8f59d58f37c221082e59342a7e
Jonah Zhu <2557928898@qq.com>
2023-05-25 21:49:50 +0800

    use geometry in anadata in intag/inrec

--- Algorithms/Tracking/src/Utils/Util.cpp
+++ Algorithms/Tracking/src/Utils/Util.cpp
@@ -7,15 +7,26 @@
 #include "TMath.h"
 
 //................................................................................//
+//Framework
+#include "Core/AnaData.h"
+
+//................................................................................//
 //Tracking
 #include "Algo/Utils/Util.h"
 
 //If particle pass through trackers
 bool InTagTrack(double x, double y, double z)
 {
-    if(z > -607.5 && z < -7.5 &&
-       y > -100   && y <  100 &&
-       x > -50    && x <  50    )
+    double x_max = dAnaData->getCenterXTag() + 0.5*dAnaData->getLengthXTag();
+    double x_min = dAnaData->getCenterXTag() - 0.5*dAnaData->getLengthXTag();
+    double y_max = dAnaData->getCenterYTag() + 0.5*dAnaData->getLengthYTag();
+    double y_min = dAnaData->getCenterYTag() - 0.5*dAnaData->getLengthYTag();
+    double z_max = dAnaData->getCenterZTag() + 0.5*dAnaData->getLengthZTag();
+    double z_min = dAnaData->getCenterZTag() - 0.5*dAnaData->getLengthZTag();
+
+    if(z > z_min && z < z_max &&
+       y > y_min && y < y_max &&
+       x > x_min && x < x_max   )
         return true;
 
     return false;
@@ -23,9 +34,16 @@ bool InTagTrack(double x, double y, double z)
 
 bool InRecTrack(double x, double y, double z)
 {
-    if(z >  7.5 && z < 180.5 &&
-       y > -100 && y < 100   &&
-       x > -50  && x < 50      )
+    double x_max = dAnaData->getCenterXRec() + 0.5*dAnaData->getLengthXRec();
+    double x_min = dAnaData->getCenterXRec() - 0.5*dAnaData->getLengthXRec();
+    double y_max = dAnaData->getCenterYRec() + 0.5*dAnaData->getLengthYRec();
+    double y_min = dAnaData->getCenterYRec() - 0.5*dAnaData->getLengthYRec();
+    double z_max = dAnaData->getCenterZRec() + 0.5*dAnaData->getLengthZRec();
+    double z_min = dAnaData->getCenterZRec() - 0.5*dAnaData->getLengthZRec();
+
+    if(z > z_min && z < z_max &&
+       y > y_min && y < y_max &&
+       x > x_min && x < x_max   )
         return true;
 
     return false;
