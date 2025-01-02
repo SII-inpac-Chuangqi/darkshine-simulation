e09882b49682998363d2a08fc802e6a0d43687a7
Jonah Zhu <2557928898@qq.com>
2025-01-02 18:55:23 +0800

    memory safety in fitter wrapper

--- Algorithms/Tracking/include/Algo/WrappedFitter.h
+++ Algorithms/Tracking/include/Algo/WrappedFitter.h
@@ -14,16 +14,29 @@ public:
     template <class ... Args>
     void Run(KalmanFilterFitter::Config config, Args&& ... args)
     {
+        if (fitter_) delete fitter_;
+
+        method = tracking::dKalman;
         fitter_ = new KalmanFilterFitter(config, std::forward<Args>(args) ...);
     }
 
     template <class ... Args>
     void Run(RiemannFitter::Config config, Args&& ... args)
     {
+        if (fitter_) delete fitter_;
+
+        method = tracking::dRiemann;
         fitter_ = new RiemannFitter(config, std::forward<Args>(args) ...);
     }
 
+    Fitter* Retrieve()
+    {
+        return fitter_;
+    }
+
 private:
+    tracking::FitMethods method;
+
     Fitter *fitter_{nullptr};
 };
 
