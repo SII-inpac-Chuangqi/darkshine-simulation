f53c4b1425a3df8866c19f75cdc7a604f4dfda1e
Jonah Zhu <2557928898@qq.com>
2025-06-12 15:24:24 +0800

    add detector style enum

---
+++ DP_simu/include/Control/DetectorType.h
@@ -0,0 +1,7 @@
+#ifndef DSIMU_DETECTOR_TYPE_H
+#define DSIMU_DETECTOR_TYPE_H
+
+enum class TrackerStyle : int {dPlane, dStrip, dPixel};
+enum class TrackerType : int {dNone, dTagging, dRecoil};
+
+#endif // DSIMU_DETECTOR_TYPE_H
