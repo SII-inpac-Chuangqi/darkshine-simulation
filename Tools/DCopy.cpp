686816225f3bdda8ceadc1fdf0aa4bcb8656d870
Jonah Zhu <2557928898@qq.com>
2026-01-27 21:51:23 +0800

    add more options in DCopy

--- Tools/DCopy.cpp
+++ Tools/DCopy.cpp
@@ -77,12 +77,16 @@ int main(int argc, char **argv) {
     std::string tree_name = "Dark_Photon";
     std::string txt_name = "events.txt";
     std::string out_name = "copied_simu.root";
+    int max_events = -1;
+    int auto_save = 4;
 
     arg_parser::Parser parser;
     parser.Add("f,file", file_name, "dp_simu.root", "name of the file to copy");
     parser.Add("t,tree", tree_name, "Dark_Photon", "name of the tree storing DEvents");
     parser.Add("x,txt", txt_name, "events.txt", "name of the txt file containing event ids");
     parser.Add("o,output", out_name, "copied_simu.root", "name of the file copied");
+    parser.Add("e,event", max_events, -1, "max events to be checked");
+    parser.Add("a,auto_save", auto_save, 4, "number of events to be auto-saved");
 
     parser.Parse(argc, argv);
 
@@ -105,9 +109,20 @@ int main(int argc, char **argv) {
 
     auto matched_dis = 0u;
     auto matched_it = events.begin();
-    for (auto i_entry = 0; i_entry < input_tree->GetEntries(); i_entry++)
+    auto n_entries = (max_events < 0 or max_events > input_tree->GetEntries()) ? input_tree->GetEntries()
+                                                                               : max_events;
+    int processed_ratio = -1;
+    int processing_ratio = 0;
+    for (auto i_entry = 0; i_entry < n_entries; i_entry++)
     {
         if (i_entry > events.back()) break;
+
+        processing_ratio = (i_entry == n_entries - 1) ? 100 : 100.*i_entry/n_entries;
+        if (processing_ratio % 10 == 0 and processing_ratio != processed_ratio)
+        {
+            processed_ratio = processing_ratio;
+            std::cout << "[INFO] ==> " << processing_ratio << "% events checked" << std::endl; 
+        }
 
         input_tree->GetEntry(i_entry);
 
@@ -117,9 +132,9 @@ int main(int argc, char **argv) {
             matched_dis = matched_it - events.begin();
 
             output_tree->Fill();
-        }
 
-        if (matched_dis % 4 == 1) output_tree->AutoSave("SaveSelf");
+            if (matched_dis % auto_save == 1) output_tree->AutoSave("SaveSelf");
+        }
     }
 
     output_tree->Write(tree_name.c_str());
