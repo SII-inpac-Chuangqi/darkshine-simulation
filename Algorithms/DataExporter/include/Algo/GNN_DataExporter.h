a29b6d5ca0eed7523f9167c53cb1f63cdfbca789
Jonah Zhu <2557928898@qq.com>
2026-05-25 01:41:47 +0000

    merge master

--- Algorithms/DataExporter/include/Algo/GNN_DataExporter.h
+++ Algorithms/DataExporter/include/Algo/GNN_DataExporter.h
@@ -6,6 +6,8 @@
 #define DSIMU_DataExporter_H
 
 #include <utility>
+#include <string>
+
 #include <TFile.h>
 #include <TTree.h>
 
@@ -42,7 +44,9 @@ private:
 
     int verbose{};
     double eps{};
+    std::string suffix;
     std::string arg_collections;
+
     TFile *f{};
     TTree *t{};
 
@@ -52,9 +56,12 @@ private:
 
     long long run_num{};
     long long evt_num{};
+    int truth_N_track_tag{0};
+    int truth_N_track_rec{0};
     std::vector<std::string> collections;
     std::map<std::string, std::map<std::string, std::vector<double> > > node;
     std::map<std::string, std::map<std::string, std::vector<size_t> > > edge;
+    std::map<std::string, std::vector<double> > energy;
     std::map<std::string, double> weight;
 };
 
